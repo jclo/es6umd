df97a5ab73e7af07d0635631bcad59d5748fa2df
jclo <jc006@wanadoo.fr>
2015-11-20 09:31:57 +0100

    Updated dependencies.

--- bin/es6umd.js
+++ bin/es6umd.js
@@ -261,9 +261,6 @@ function _populate(opts) {
   // Ok. Populate it.
   console.log('Populates the folder with:');
 
-  // README.md LICENSE.md, CHANGELOG.md, index.js, .eslintrc, .gitignore, .travis.yml
-  // folders test & lib
-
   // Create README.md, LICENSE.md, CHANGELOG.md
   console.log('  ' + 'README.md');
   fs.writeFileSync(path.join(baseapp, 'README.md'), readme);
